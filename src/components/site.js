faff4ac54355d4b7f79672cd937b3d41d3112300
Pholey <pholeyoh@gmail.com>
2015-12-18 00:11:00 -0800

    Fix routes

--- src/components/site.js
+++ src/components/site.js
@@ -2,7 +2,7 @@ import React from "react"
 import cx from "classnames"
 import {SiteHeader} from "./header"
 
-export class Site extends React.Component {
+export default class Site extends React.Component {
   constructor() {
     super(...arguments)
     this.state = {
@@ -16,13 +16,15 @@ export class Site extends React.Component {
 
   render() {
     let expandedClass = this.state.hamburgerOpen ? "expanded" : ""
-
+    console.log("props!", this.props)
     return (
       <div className="site">
         <SiteHeader onToggleHamburger={this.onToggleHamburger} title="lel" />
         <div className="site-content">
           <div className={cx("hamburger-menu", expandedClass)}></div>
-          <div className="page-container">{this.props.children}</div>
+          <div className="page-container">
+            {this.props.children}
+          </div>
         </div>
       </div>
     )
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,15 @@ import React from "react"
 import {Route, IndexRoute} from "react-router"
 
 import Home from "./pages/home"
+import Site from "./components/site"
 
 import {Test1, Test2} from "./pages/test"
 
 export default (flux) => {
   return (
     <Route>
-      <Route component={Home} path="/">
+      <Route component={Site} path="/" >
+        <IndexRoute component={Home}/>
         <Route component={Test1} path="/test1" />
         <Route component={Test2} path="/test2" />
       </Route>
